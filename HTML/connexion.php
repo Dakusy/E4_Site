7a19095838504024f0da9358c7c7726e05752d5f
Tourdot Thomas <thomas.tourdot1999@gmail?com>
2019-09-30 12:03:30 +0200

    modif connexion.php
    quasi rien

--- HTML/connexion.php
+++ HTML/connexion.php
@@ -1,6 +1,7 @@
 <?php
 try{
-    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd = new PDO('mysql:host=access799985010.webspace-data.io;dbname=dbs179815;charset=utf8', 'u98724108', 'Hitler59110*', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    echo 'reussie';
 }
 catch(Exception $e){
     die('Erreur : '.$e->getMessage());
