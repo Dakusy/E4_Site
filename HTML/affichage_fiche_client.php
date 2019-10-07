4d0610e3b2b62d920c9672892dcd6532389d1cbd
Tourdot Thomas <thomas.tourdot1999@gmail?com>
2019-10-07 11:42:01 +0200

    Modif des 2 fichiers

--- HTML/affichage_fiche_client.php
+++ HTML/affichage_fiche_client.php
@@ -1,9 +1,30 @@
 <?php 
 session_start();
 session_unset();
+include 'connexion.php';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
     <body>
+        <?php
+        $requete=$bdd->query('SELECT * FROM Client');
+
+        while($donnees=$requete->fetch()){
+            echo $donnees['numClient'];
+            echo $donnees['raisonSociale'];
+            echo $donnees['numSIREN'];
+            echo $donnees['codeAPE'];
+            echo $donnees['adressePostale'];
+            echo $donnees['numTelephone'];
+            echo $donnees['numTelecopie'];
+            echo $donnees['adresseMail'];
+            echo $donnees['distanceAgence'];
+            echo $donnees['dureeTrajet'];
+            echo $donnees['numAgence'];
+        }
+        ?>
     </body>
+    <?php 
+    include 'bootstrap.php';
+    ?>
 </html>
--- HTML/connexion.php
+++ HTML/connexion.php
@@ -1,7 +1,6 @@
 <?php
 try{
     $bdd = new PDO('mysql:host=access799985010.webspace-data.io;dbname=dbs179815;charset=utf8', 'u98724108', 'Hitler59110*', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-    echo 'reussie';
 }
 catch(Exception $e){
     die('Erreur : '.$e->getMessage());
