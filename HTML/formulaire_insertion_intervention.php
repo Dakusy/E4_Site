70c126b3dffe07527a43c480511349dad8cace0a
Dakusy <dakusy@gmail.com>
2019-11-18 11:00:06 +0100

    UPDATE INSERTION + PDF [WORKING]

---
+++ HTML/formulaire_insertion_intervention.php
@@ -0,0 +1,52 @@
+<!doctype html> 
+<html> 
+<head> 
+<meta charset="utf-8"> 
+        <link rel="stylesheet" href="../CSS/formulaire.css">
+<title>test</title> 
+</head> 
+
+<body> 
+    
+   <h1>Formulaire</h1> 
+<br>
+    <p><i>Complétez le formulaire. Les champs marqué par </i><em>*</em> sont <em>obligatoires</em></p>
+    
+<center><fieldset><form ACTION="insertion_intervention_bdd.php" METHOD="post"> 
+
+    <legend><h2>Fiche D'intervention</h2></legend>
+    
+<em>*</em> Numero : <input type="number" name="numIntervention" required = "">
+    
+<br>
+    
+<em>*</em> Date : <input type="date" name = "date" required = "">
+    
+<br>
+    
+<em>*</em> Heure : <input type="text" name = "heure" required = "" placeholder = "hh:mm">
+
+<br>
+    
+<em>*</em> tempsIntervention : <input type = "text" name= "tempsIntervention" required = "" placeholder = "hh:mm">
+    
+<br>
+    
+<em>*</em> commentaireTechnicien : <input type = "text" name = "commentaireTechnicien" required = "">
+    
+<br>
+    
+<em>*</em> numClient : <input type = "number" name = "numClient" required = "">
+    
+<br>
+    
+<em>*</em> numEmploye : <input type = "number" name = "numEmploye" required = "">
+    
+<br>
+    <br>
+    <p><input type = "reset" >
+    <input type="submit" value="insertion"> </p>
+    
+    </form> </fieldset></center>
+</body> 
+</html>
--- HTML/insertion_intervention_bdd.php
+++ HTML/insertion_intervention_bdd.php
@@ -4,26 +4,62 @@ include 'connexion.php';
 
 if(isset($_POST['numIntervention']))
 {
-$nom = $_POST["numIntervention"]; 
-$date = $_POST["date"];
-$heure = $_POST["heure"];
-$tempsIntervention = $_POST["tempsIntervention"];
-$commentaireTechnicien = $_POST["commentaireTechnicien"];
-$numClient = $_POST["numClient"];
-$numEmploye = $_POST["numEmploye"];
+$_SESSION['numIntervention'] = $_POST["numIntervention"]; 
+$_SESSION['date'] = $_POST["date"];
+$_SESSION['heure'] = $_POST["heure"];
+$_SESSION['tempsIntervention'] = $_POST["tempsIntervention"];
+$_SESSION['commentaireTechnicien'] = $_POST["commentaireTechnicien"];
+$_SESSION['numClient'] = $_POST["numClient"];
+$_SESSION['$numEmploye'] = $_POST["numEmploye"];
 }
 
-$req = $bdd->prepare("INSERT INTO intervention (numIntervention, date, heure, tempsIntervention, commentaireTechnicien, numClient, numEmploye) VALUES (:numIntervention, :date, :heure, :tempsIntervention, :commentaireTechnicien, :numClient, :numEmploye)");
+$req = $bdd->prepare("SELECT numIntervention FROM Intervention WHERE numIntervention = :numIntervention ");
 	$req->execute(array(
-			"numIntervention" => $nom,
-			"date" => $date,
-			"heure" => $heure,
-			"tempsIntervention" => $tempsIntervention,
-			"commentaireTechnicien" => $commentaireTechnicien,
-			"numClient" => $numClient,
-			"numEmploye" => $numEmploye
+			"numIntervention" => $_SESSION['numIntervention']
 			));
- 
+			
+$numInterventionExist = $req->rowCount();
 
+if($numInterventionExist == 0){
+$req2 = $bdd->prepare("INSERT INTO Intervention (numIntervention, date, heure, tempsIntervention, commentaireTechnicien, numClient, numEmploye) VALUES (:numIntervention, :date, :heure, :tempsIntervention, :commentaireTechnicien, :numClient, :numEmploye)");
+	$req2->execute(array(
+			"numIntervention" => $_SESSION['numIntervention'],
+			"date" => $_SESSION['date'],
+			"heure" => $_SESSION['heure'],
+			"tempsIntervention" => $_SESSION['tempsIntervention'],
+			"commentaireTechnicien" => $_SESSION['commentaireTechnicien'],
+			"numClient" => $_SESSION['numClient'],
+			"numEmploye" => $_SESSION['$numEmploye']
+			));
+}
 
+else{
+	
 ?>
+
+<script>
+alert("Erreur : Numéro d'Intervention déjà existant, vous allez être redirigé vers le formulaire");
+document.location.href="formulaire_insertion_intervention.php";
+</script>
+
+<?php
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <body>
+	<center><form ACTION="pdf.php" METHOD="post">
+	<p>Voulez vous génerer un PDF lié à votre Intervention ou ultérieurement ?
+	
+	<input type="submit" value="Oui" >
+	</form>
+	
+	<form ACTION="main.html" METHOD="post">
+	<input type="submit" value="Non"></center></p>
+	</form>
+	
+	</body>
+</html>
+
