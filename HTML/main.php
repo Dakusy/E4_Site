22a45d01eefeb775cb4cca512e90aee5d63f23f0
Tourdot Thomas <thomas.tourdot1999@gmail?com>
2019-11-18 09:06:30 +0100

    Modif de l'affichage d'une fiche client visualiser

--- HTML/main.php
+++ HTML/main.php
@@ -14,7 +14,7 @@ session_unset();
                 <section class = "col-lg-4 offset-lg-4">
                     <br><br>
                     <div class = "jumbotron">
-                        <form method="post" action="">
+                        <form method="post" action="affichage_fiche_client.php">
                             <h1>
                                 Connexion CashCash
                             </h1>
