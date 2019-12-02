627a46329c31325ac6388060a3c7169e7b74610c
Varingot Tom <varingot.tom@gmail.com>
2019-12-02 10:20:37 +0100

    bouton consulter, fiche intervention technicien + page de validation

--- HTML/consulter_intervention_technicien.php
+++ HTML/consulter_intervention_technicien.php
@@ -4,45 +4,61 @@ include 'connexionTechnicien.php';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+    <?php
+
+    ?>
     <body>
         <center>
             <div class = "row">
                 <section class = "col-lg-4 offset-lg-4">
                     <br><br>
-                    <div class = "jumbotron">
-                        <div class= "offset-2">
-                            <div class="form-group row">
-                                <form action="traitement_fiche_intervention.php" method="post">
-                                    <div class="col-lg-8">
+                    <center>
+                        <div class = "jumbotron">
+
+
+                            <form action="./consulter_intervention_technicien.php" method="post">
+                                <div class="col-lg-8">
+                                    <div class="input-group">
+                                        <!-- Reparer cette barre de recherche  pour qu'elle recherche bien le num cherché -->
                                         <label>Numéro d'intervention: </label>
-                                        <input type="text" name="numIntervention" value=""><br>
-                                        <br>
-                                        <!-- Consultation des interventions par un technicien -->
-                                        <select multiple class="form-control col-12" id="exampleFormControlSelect2" size = 10 style = "width: auto" name="numCLient">
-                                            <?php
-                                            $idTech="SELECT numEmploye 
+                                        <input type="text" name="numIntervention" value="" placeholder="...">
+                                        <div class="input-group-btn">
+                                            <button type="submit" class="btn btn-primary">
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <form action="./traitement_fiche_intervention.php" method="post">
+
+                                <select multiple class="form-control col-12" id="exampleFormControlSelect2" size = 10 style = "width: auto" name="numIntervention">
+                                    <?php
+                                    $idTech="SELECT numEmploye 
                                             FROM technicien t, users u 
                                             WHERE u.proprietaire=t.numEmploye";
-                                            $rqIdTech=$bdd->query($idTech);
-                                            $idTech=$rqIdTech->fetch();
-                                            $numIntervention="SELECT numIntervention 
+                                    $rqIdTech=$bdd->query($idTech);
+                                    $idTech=$rqIdTech->fetch();
+                                    $numIntervention="SELECT numIntervention 
                                             FROM intervention i INNER JOIN technicien t ON i.numEmploye=t.numEmploye 
                                             WHERE t.numEmploye=".$idTech['numEmploye']." 
                                             ORDER BY date ASC";
-                                            $rqnumIntervention=$bdd->query($numIntervention);
-                                            while($result=$rqnumIntervention->fetch()){
-                                                echo "<option>".$result['numIntervention']."</option>";
-                                            }
-                                            ?>
-                                        </select>
-                                        <br>
-                                        <input type="submit" value="Consulter" name="Consulter">
-                                        
-                                    </div>
-                                </form>
-                            </div>
+                                    $rqnumIntervention=$bdd->query($numIntervention);
+                                    while($result=$rqnumIntervention->fetch()){
+                                        echo "<option>".$result['numIntervention']."</option>";
+                                    }
+                                    ?>
+                                </select>
+                                <br>
+                                <button type="submit" name="Consulter">Consulter</button>
+                            </form>
+                            <form action="./validation_fiche_intervention.php" method="post">
+                                <button type="submit" name="Valider">Valider</button>
+                            </form>
+
+
+
                         </div>
-                    </div>
+                    </center>
                 </section>
             </div>
         </center>
